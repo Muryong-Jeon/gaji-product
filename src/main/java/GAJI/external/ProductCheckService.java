f9391c8156c0fdc944d3902da88c5b0ef09bbc2e
Muryong-Jeon <muryongjeon@gmail.com>
2020-09-09 14:54:42 +0900

    first commit

---
+++ src/main/java/GAJI/external/ProductCheckService.java
@@ -0,0 +1,15 @@
+
+package GAJI.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name="productcheck", url="{api.url.productcheck}")
+public interface ProductCheckService {
+
+    @RequestMapping(method= RequestMethod.POST, path="/productChecks")
+    public void requestCheck(@RequestBody ProductCheck productCheck);
+
+}
